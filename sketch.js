bb2ece81851350c100d3d6bfb121c7247f128f4f
craftycram <github@craftycram.net>
2021-06-17 15:45:12 +0200

    added sound gesture control

--- sketch.js
+++ sketch.js
@@ -4,6 +4,12 @@ const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
 let updateNote = document.getElementById("updatenote");
 
+let song;
+let songIndex = 0;
+const songTitles = ['iot.mp3', 'dsmn.mp3', 'hge.mp3'];
+const songs = [];
+let volume = 0.5;
+
 let isVideo = false;
 let model = null;
 let eX = 0;
@@ -51,7 +57,7 @@ function toggleVideo() {
 
 function runDetection() {
     model.detect(video).then(predictions => {
-        const filteredPredictions = predictions.filter((p) => p.label === 'open' || p.label === 'closed' || p.label === 'point');
+        const filteredPredictions = predictions.filter((p) => p.label === 'open' || p.label === 'pinch' || p.label === 'point' || p.label === 'closed');
 
         let current;
         // ignore empty array filteredPredictions as current is undefined
@@ -59,38 +65,71 @@ function runDetection() {
         if (filteredPredictions.length === 2) current = filteredPredictions.sort((a, b) => a.bbox[0] < b.bbox[0])[0];
         if (current) stateHistory.push(current.label);
         if (stateHistory.length >= 4) stateHistory = stateHistory.slice(-4) || [];
-        console.log(stateHistory);
-        if (stateHistory.filter((e) => stateHistory[0] === e).length === 4) console.log('tada');
+        // console.log(stateHistory);
+        // if (stateHistory.filter((e) => stateHistory[0] === e).length === 4) console.log('tada');
         
         const vidW = document.getElementById('myvideo').width;
         const vidH = document.getElementById('myvideo').height;
 
+        const detected = stateHistory.filter((e) => stateHistory[0] === e).length === 4 ? stateHistory[0] : false;
+        console.log(detected);
         
-        if (current) {
+        if (detected && current) {
 
           vX = map(current.bbox[0], 0, vidW, 0, width / 2);
           tX = current.bbox[0];
           vY = map(current.bbox[1], 0, vidH, 0, height / 2);
           tY = current.bbox[1];
 
-          if (current.label === 'closed') {
+          // reset
+          if (detected === 'point') {
             eX = width/2;
             eY = height/2;
+            if (!song.isPlaying()) song.play();
+          }
+          if (detected === 'closed') {
+            eX = width/2;
+            eY = height/2;
+            if (song.isPlaying()) song.pause();
           }
           
-          if (lastState === 'open' && current.label === 'point') {
+          // start
+          if (lastState === 'open' && detected === 'pinch') {
+            console.log('start');
             startX = vX;
             startY = vY;
           }
   
-          if (lastState === 'point' && current.label === 'open') {
+          // end
+          if (lastState === 'pinch' && detected === 'open') {
+            console.log('end');
             eX += (vX - startX);
             eY += (vY - startY);
+            const dirVH = Math.abs(vX - startX) - Math.abs(vY - startY) > 0 ? 'h' : 'v';
+            console.log(dirVH);
+            switch (dirVH) {
+              case 'h':
+                const next = vX - startX > 0 ? +1 : -1;
+                console.log('n',next);
+                songIndex += next;
+                if (songIndex < 0) songIndex = songs.length + songIndex;
+                if (songIndex > songs.length - 1) songIndex = 0;
+                song.stop();
+                song = songs[songIndex];
+                if (!song.isPlaying()) song.play();
+                break;
+              case 'v':
+                volume -= map(vY - startY, 0, vidH / 2, 0, 1);
+                song.setVolume(volume);
+                console.log(volume);
+                break;
+              default:
+                break;
+            }
           }
+
+          lastState = detected;
         }
-
-
-        lastState = current ? current.label : 'none';
         // console.log(predictions);
         model.renderPredictions(predictions, canvas, context, video);
         if (isVideo) {
@@ -107,8 +146,14 @@ handTrack.load(modelParams).then(lmodel => {
     trackButton.disabled = false
 });
 
+function preload() {
+  for (let i = 0; i < songTitles.length; ++i)
+    songs[i] = loadSound(`assets/${songTitles[i]}`);
+}
 
 function setup() {
+  song = loadSound('assets/iot.mp3');
+  song.setVolume(volume);
   createCanvas(640, 480);
   eX = width / 2;
   eY = height / 2;
