c0e68b1b9a7b3da8c58dc4cb7028bd2334726f30
craftycram <github@craftycram.net>
2021-06-21 09:17:42 +0200

    added comments

--- sketch.js
+++ sketch.js
@@ -4,12 +4,14 @@ const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
 let updateNote = document.getElementById("updatenote");
 
+// declaring sound variables
 let song;
 let songIndex = 0;
 const songTitles = ['iot.mp3', 'dsmn.mp3', 'hge.mp3'];
 const songs = [];
 let volume = 0.5;
 
+// declaring tracking variables
 let isVideo = false;
 let model = null;
 let eX = 0;
@@ -21,6 +23,7 @@ let xDif, yDif;
 let lastState = '';
 let stateHistory = [];
 
+// configuring model
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video  
     maxNumBoxes: 20,        // maximum number of boxes to detect
@@ -28,6 +31,7 @@ const modelParams = {
     scoreThreshold: 0.6,    // confidence threshold for predictions.
 }
 
+// start video stream & detection
 function startVideo() {
     handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
@@ -41,6 +45,7 @@ function startVideo() {
     });
 }
 
+// toggle webcam stream
 function toggleVideo() {
     if (!isVideo) {
         updateNote.innerText = "Starting video"
@@ -53,14 +58,15 @@ function toggleVideo() {
     }
 }
 
-
-
+// run handtrack detection
 function runDetection() {
     model.detect(video).then(predictions => {
+        // filter needed cases
         const filteredPredictions = predictions.filter((p) => p.label === 'open' || p.label === 'pinch' || p.label === 'point' || p.label === 'closed');
 
         let current;
         // ignore empty array filteredPredictions as current is undefined
+        // get relevant hand
         if (filteredPredictions.length === 1) current = filteredPredictions[0];
         if (filteredPredictions.length === 2) current = filteredPredictions.sort((a, b) => a.bbox[0] < b.bbox[0])[0];
         if (current) stateHistory.push(current.label);
@@ -68,49 +74,58 @@ function runDetection() {
         // console.log(stateHistory);
         // if (stateHistory.filter((e) => stateHistory[0] === e).length === 4) console.log('tada');
         
+        // get video resolution
         const vidW = document.getElementById('myvideo').width;
         const vidH = document.getElementById('myvideo').height;
 
+        // get state with time buffer
         const detected = stateHistory.filter((e) => stateHistory[0] === e).length === 4 ? stateHistory[0] : false;
         console.log(detected);
         
+        // check if got a valid detection
         if (detected && current) {
 
+          // calculate coordinates & map to video size
           vX = map(current.bbox[0], 0, vidW, 0, width / 2);
           tX = current.bbox[0];
           vY = map(current.bbox[1], 0, vidH, 0, height / 2);
           tY = current.bbox[1];
 
-          // reset
+          // resume playback hand pose
           if (detected === 'point') {
             eX = width/2;
             eY = height/2;
             if (!song.isPlaying()) song.play();
           }
+          // stop playback handpose
           if (detected === 'closed') {
             eX = width/2;
             eY = height/2;
             if (song.isPlaying()) song.pause();
           }
           
-          // start
+          // start volume & skip gesture detection
           if (lastState === 'open' && detected === 'pinch') {
             console.log('start');
             startX = vX;
             startY = vY;
           }
   
-          // end
+          // end volume & skip gesture detection
           if (lastState === 'pinch' && detected === 'open') {
             console.log('end');
             eX += (vX - startX);
             eY += (vY - startY);
+            // check horizontal vs vertical gesture direction
             const dirVH = Math.abs(vX - startX) - Math.abs(vY - startY) > 0 ? 'h' : 'v';
             console.log(dirVH);
             switch (dirVH) {
+              // horizontal gesture
               case 'h':
+                // check if left or right
                 const next = vX - startX > 0 ? +1 : -1;
                 console.log('n',next);
+                // skip song & resume playing if stopped
                 songIndex += next;
                 if (songIndex < 0) songIndex = songs.length + songIndex;
                 if (songIndex > songs.length - 1) songIndex = 0;
@@ -118,7 +133,9 @@ function runDetection() {
                 song = songs[songIndex];
                 if (!song.isPlaying()) song.play();
                 break;
+              // vertical gesture
               case 'v':
+                // change volume
                 volume -= map(vY - startY, 0, vidH / 2, 0, 1);
                 song.setVolume(volume);
                 console.log(volume);
@@ -127,10 +144,10 @@ function runDetection() {
                 break;
             }
           }
-
+          // store laste detected gesture
           lastState = detected;
         }
-        // console.log(predictions);
+        // render prediction on frame
         model.renderPredictions(predictions, canvas, context, video);
         if (isVideo) {
             requestAnimationFrame(runDetection);
@@ -147,11 +164,13 @@ handTrack.load(modelParams).then(lmodel => {
 });
 
 function preload() {
+  // load all sound files
   for (let i = 0; i < songTitles.length; ++i)
     songs[i] = loadSound(`assets/${songTitles[i]}`);
 }
 
 function setup() {
+  // load initial sound file
   song = loadSound('assets/iot.mp3');
   song.setVolume(volume);
   createCanvas(640, 480);
@@ -160,6 +179,7 @@ function setup() {
 }
 
 function draw() {
+  // draw canvas
   if (lastState === 'none' || lastState === 'closed') background(220);
   background(220);
   noStroke();
